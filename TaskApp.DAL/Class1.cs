d3b1684da8eba8b03108d6d57516f6a4be103dd4
Sarojini Maddaraki <sarojinimaddaraki62@gmail.com>
2026-05-01 09:50:53 +0530

    TaskApp.DAL Added

---
+++ TaskApp.DAL/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskApp.DAL
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ TaskApp.DAL/TaskApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskApp.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskApp.DAL
+{
+    public class TaskApplicationContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(local)\\SQLEXPRESS;Database=TaskToDo;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        //public TaskApplicationContext()
+        //{
+
+        //}
+
+        public TaskApplicationContext(DbContextOptions<TaskApplicationContext>options) : base(options)
+        {
+
+        }
+        public DbSet<TaskModel> Tasks { get; set; }
+        public DbSet<UserModel> Users{ get; set; }
+        public DbSet<CategoryModel> Categories { get; set; }
+
+    }
+}
